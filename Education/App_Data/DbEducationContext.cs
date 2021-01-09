e57c4902920e49628685ef6a68728ed56196ffe1
sarancha133122@gmail.com <samsung>
2021-01-09 14:03:19 +0300

    Первая миграция и создание БД

--- Education/App_Data/DbEducationContext.cs
+++ Education/App_Data/DbEducationContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Education.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,21 @@ namespace Education.App_Data
 
         public DbEducationContext(DbContextOptions<DbEducationContext> options) : base(options) { }
 
+        public DbSet<Avto> Avtos { get; set; }
+        public DbSet<AvtoPark> AvtoParks  { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Events> Events { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Federation> Federations { get; set; }
+        public DbSet<Olimpic> Olimpics { get; set; }
+        public DbSet<OlimpicAndStudent> OlimpicAndStudents { get; set; }
+        public DbSet<OtherPersonal> OtherPersonals { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Salary> Salaries { get; set; }
+        public DbSet<School> Schools { get; set; }
+        public DbSet<Sport> Sports { get; set; }
+        public DbSet<Sportsman> Sportsmen { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
     }
 }
